654d5ae9d1e7a35c962b3426505b9eb58bdf08e5
Rocio Hidalgo Bini <hidalgobinirocio@gmail.com>
2022-09-23 16:21:30 -0300

    ignore

---
+++ ejemploClase3/ejemploClase3/Cuenta.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemploClase3
+{
+    internal class Cuenta
+    {
+        int monto;
+        public enum tipoExtraccion {
+            cajeroHumano = 1,
+            cajeroAutomatico = 2
+        };
+        public enum tipoCuenta
+        {
+            corrientePesos = 1,
+            corrienteDolares = 2,
+            cajaAhorro = 3
+        };
+
+        public void insercion(int monto)
+        {
+
+        }
+        public void extraccion(int monto, tipoExtraccion tipo)
+        {
+
+        }
+    }
+}
